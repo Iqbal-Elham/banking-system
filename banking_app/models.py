05c43f93fe2c6db52dd24b7e7c408ea5eb521101
iqbal elham <iqbal.elham7@gmail.com>
2023-07-30 16:17:09 +0430

    Add account, transactions and notifications model

--- banking_app/models.py
+++ banking_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    # Add any additional fields you need for the user model
+    # For example, you can add fields like address, phone number, etc.
+    pass
+
+class Account(models.Model):
+    ACCOUNT_TYPES = (
+        ('savings', 'Savings'),
+        ('checking', 'Checking'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_type = models.CharField(max_length=10, choices=ACCOUNT_TYPES)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('deposit', 'Deposit'),
+        ('withdrawal', 'Withdrawal'),
+        ('transfer', 'Transfer'),
+    )
+
+    sender = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='sender_transactions')
+    receiver = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='receiver_transactions', null=True, blank=True)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
