829f7ac9bf1f2eb39a0a217be82b1c5bd354c984
iqbal elham <iqbal.elham7@gmail.com>
2023-07-31 16:21:10 +0430

    Migrate the model to database

--- banking_app/models.py
+++ banking_app/models.py
@@ -5,29 +5,29 @@ from django.contrib.auth.models import AbstractUser
 #     # address, phone number, etc.
 #     pass
 
-# class Account(models.Model):
-#     ACCOUNT_TYPES = (
-#         ('savings', 'Savings'),
-#         ('checking', 'Checking'),
-#     )
+class Account(models.Model):
+    ACCOUNT_TYPES = (
+        ('savings', 'Savings'),
+        ('checking', 'Checking'),
+    )
 
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     account_type = models.CharField(max_length=10, choices=ACCOUNT_TYPES)
-#     balance = models.DecimalField(max_digits=10, decimal_places=2)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_type = models.CharField(max_length=10, choices=ACCOUNT_TYPES)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
 
-# class Transaction(models.Model):
-#     TRANSACTION_TYPES = (
-#         ('deposit', 'Deposit'),
-#         ('withdrawal', 'Withdrawal'),
-#         ('transfer', 'Transfer'),
-#     )
-#     sender = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='sender_transactions')
-#     receiver = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='receiver_transactions', null=True, blank=True)
-#     transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
-#     amount = models.DecimalField(max_digits=10, decimal_places=2)
-#     date = models.DateTimeField(auto_now_add=True)
+class Transaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('deposit', 'Deposit'),
+        ('withdrawal', 'Withdrawal'),
+        ('transfer', 'Transfer'),
+    )
+    sender = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='sender_transactions')
+    receiver = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='receiver_transactions', null=True, blank=True)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
 
-# class Notification(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     message = models.TextField()
-#     date = models.DateTimeField(auto_now_add=True)
+class Notification(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
