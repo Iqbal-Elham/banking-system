c919902fdf59c65e9382f756d36f653eebb350d5
iqbal elham <iqbal.elham7@gmail.com>
2023-08-02 11:33:52 +0430

    Add transaction in url and settings app

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'banking_app',
+    'transactions',
 ]
 
 MIDDLEWARE = [
--- banking_system/urls.py
+++ banking_system/urls.py
@@ -22,5 +22,9 @@ from banking_app.views import HomeView
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path(
+        'transactions/',
+        include('transactions.urls', namespace='transactions')
+    )
 ]
